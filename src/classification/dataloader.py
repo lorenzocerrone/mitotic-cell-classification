1e3e68bca1843ac39ea79eda4c902085a2bdb9b8
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2022-06-02 17:13:34 +0200

    fix data loader

--- src/classification/dataloader.py
+++ src/classification/dataloader.py
@@ -5,8 +5,9 @@ import torch
 
 class PatchDataset2D(Dataset):
     def __init__(self, list_paths, use_cache=False):
-        global_list_stack, global_num_nuclei = self.get_samples_mapping(list_paths)
+        global_list_stack, global_num_nuclei, labels = self.get_samples_mapping(list_paths)
 
+        self.labels = labels
         self.z_mid_point = self.get_z_mid_point(list_paths[0])
         self.global_num_nuclei = global_num_nuclei
         self.global_list_stack = global_list_stack
@@ -22,26 +23,36 @@ class PatchDataset2D(Dataset):
     @staticmethod
     def get_samples_mapping(list_paths):
         global_list_stack = []
+        all_labels = []
 
         for path in list_paths:
             with h5py.File(path, 'r') as f:
                 num_nuclei = f['labels'].shape[0]
+                all_labels.append(torch.from_numpy(f['labels'][...]))
                 list_stack = [(path, i) for i in range(num_nuclei)]
 
-        global_list_stack += list_stack
-        return global_list_stack, len(global_list_stack)
+            global_list_stack += list_stack
+        return global_list_stack, len(global_list_stack), torch.cat(all_labels)
 
     def get(self, idx):
         path, cell_idx = self.global_list_stack[idx]
         with h5py.File(path, 'r') as f:
             raw = f['raw_patches'][cell_idx, self.z_mid_point, ...]
-            seg = f['seg_patches'][cell_idx, self.z_mid_point, ...]
-            label = f['labels'][cell_idx]
+            # seg = f['seg_patches'][cell_idx, self.z_mid_point, ...]
 
         raw = torch.from_numpy(raw).float()
-        seg = torch.from_numpy(seg.astype('int32')).float()
+        # seg = torch.from_numpy(seg.astype('int32')).float()
 
-        return torch.stack([raw, seg], 0), label
+        # return torch.stack([raw, seg], 0), label
+        return raw, self.labels[idx]
+
+    def compute_weights(self, class_weights=None):
+        if class_weights is None:
+            _, class_weights = torch.unique(self.labels, return_counts=True)
+            class_weights = 1 / class_weights
+
+        weights = torch.where(self.labels == 0, class_weights[0], class_weights[1])
+        return weights
 
     def get_from_cache(self, idx):
         if idx in self.cache:
